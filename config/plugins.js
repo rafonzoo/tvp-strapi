0af1d2765761bf46df693f394ee8c38e1d0efba0
rafonzoo <rafaelonline.rr@gmail.com>
2023-05-16 00:12:15 +0700

    update dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,18 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
+  'users-permissions': {
+    config: {
       jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  },
+});
